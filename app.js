43ae2aedfb11361c45f6d637c22c93fa26160c9e
Hugocod <u.medioli@icloud.com>
2022-08-26 14:48:04 +0200

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+/* Genero numeri casuali da 1 a 6. */
+let humanNumber = Math.floor(Math.random() * 6 + 1);
+let computerNumber = Math.floor(Math.random() * 6 + 1);
+
+/* stampo i risultati */
+console.log(humanNumber);
+console.log(computerNumber);
+
+/* comparo i risultati e stampo il vincitore */
+if (humanNumber > computerNumber) {
+    console.log("computer won");
+} else {
+    console.log("human won");
+}
